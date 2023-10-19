8fb7e38d238b979388295d10bc7454774243cfca
Matthew <Deweasematthew@gmail.com>
2023-10-19 03:27:24 -0500

    basic array of questions fn

--- index.js
+++ index.js
@@ -7,49 +7,95 @@ const { type } = require('os');
 const questions = [
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "projectName",
+    message: "Whats the name of the project?",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "description",
+    message: "summary of the project and what it entails:",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "installation",
+    message: "how do the users install this project?",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "usage",
+    message: "provide a usage example:",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "screenshots",
+    message: "enter path to screenshot (if any);",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "technologies",
+    message: "list what technologies/languages used:",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "contribution",
+    message: "how can others help contribute:",
   },
 
   {
     type: "input",
-    name: "title",
-    message: "enter name of project",
+    name: "testing",
+    message: "describe how to test the project:",
   },
+  {
+    type: "list",
+    name: "license",
+    message: "choose a license for the project:",
+    choices: ["MIT"],
+    // add more later when needed//
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "list the contributors who helped with the project:",
+  },
+  {
+    type: "input",
+    name: "contact",
+    message: "provide contact information:",
+  },
+  {
+    type: "input",
+    name: "badges",
+    message: "list badges (if any):",
+  },
+
+  {
+    type: "input",
+    name: "roadmap",
+    message: "describe any improvements that will be added:",
+  },
+  {
+    type: "input",
+    name: "faq",
+    message: "list questions that could be asked frequently (if any):",
+  },
+  {
+    type: "input",
+    name: "acknowledgments",
+    message: "type any acknowledgments and thanks?",
+  },
+  {
+    type: "input",
+    name: "dependencies",
+    message: "list project dependencies:",
+  },
+
 ];
+
+module.exports = questions;
