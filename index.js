6821124fb22fbfe5ede15ab54bc485e4bdd9e5b0
Matthew <Deweasematthew@gmail.com>
2023-10-19 03:02:39 -0500

    added basic array

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "enter name of project",
+  },
+];
